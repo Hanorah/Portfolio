bc0e8309b92dda99a3af550a06a1ae83d13c1049
Hanorah <ksdnorah@gmail.com>
2025-02-24 20:54:13 +0100

    Initial commit

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,138 +1,95 @@
-
-import React, { useContext } from 'react';
-
-
-import { Header } from '../Layout/style';
-import Link from '../ActiveLink';
-import AppContext from '../Utils/context';
-
-
-import { Logo, Moon, Icon } from '../Icons';
+import React, { useContext } from "react";
+import { Header } from "../Layout/style";
+import Link from "../ActiveLink";
+import AppContext from "../Utils/context";
+import { Logo, Moon, Icon } from "../Icons";
 
 const Navbar = () => {
-  const { show, handleopen, setTheme, closeShow, theme } = useContext(
-    AppContext
-  );
+  const { show, handleopen, setTheme, closeShow, theme } = useContext(AppContext);
 
   return (
-    <>
-      <Header>
-        <nav className="navbar navbar-expand-lg bg-light navbar-light">
-          <div className="container">
-            <Link
-              href="/"
-              className="navbar-brand"
-              aria-label="Hanorah Home"
-              tabIndex={show ? -1 : undefined}
-            >
-              <Logo />
-            </Link>
+    <Header>
+      <nav className="navbar navbar-expand-lg bg-light navbar-light">
+        <div className="container">
+          {/* Logo */}
+          <Link href="https://portfolio-pco6.onrender.com/" className="navbar-brand" aria-label="Hanorah Home">
+            <Logo />
+          </Link>
 
-            <button
-              className="navbar-toggler pr-0"
-              type="button"
-              onClick={handleopen}
-              tabIndex={show ? -1 : undefined}
-              aria-label="Open Button Toggle"
-            >
-              <span className="navbar-toggler-icon" />
+          {/* Mobile Toggle Button */}
+          <button
+            className="navbar-toggler pr-0"
+            type="button"
+            onClick={handleopen}
+            aria-label="Open Navigation Menu"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          {/* Navbar Links */}
+          <div className={`collapse navbar-collapse ${show ? "show" : ""}`} id="collapsibleNavbar">
+            {/* Close Button for Mobile */}
+            <button className="d-block d-md-none close-nav" onClick={handleopen} type="button">
+              <Icon />
             </button>
 
-            <div
-              className={`collapse navbar-collapse  ${show && 'show'}`}
-              id="collapsibleNavbar"
-            >
-              <button
-                className="d-block d-md-none close-nav"
-                onClick={handleopen}
-                type="button"
-              >
-                <Icon />
-              </button>
+            <ul className="navbar-nav ml-auto">
+              {/* Home Link */}
+              <li className="nav-item hover__bottom">
+                <Link
+                  href="https://portfolio-pco6.onrender.com/"
+                  activeClassName="is-active"
+                  className="nav-link"
+                  id="cardHover"
+                  onClick={closeShow}
+                  aria-label="Go Home"
+                  title="Home"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Home
+                </Link>
+              </li>
 
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item hover__bottom d-block d-md-none">
-                  <Link
-                    href="/"
-                    activeClassName="is-active"
-                    className="nav-link"
-                    id="cardHover"
-                    onClick={closeShow}
-                    aria-label="Go Home"
-                    title="Home"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item hover__bottom">
-                  <Link
-                    href="/about"
-                    activeClassName="is-active"
-                    className="nav-link"
-                    id="cardHover"
-                    onClick={closeShow}
-                    aria-label="Go To About Page"
-                    title="About"
-                  >
-                    About
-                  </Link>
-                </li>
-                <li className="nav-item hover__bottom">
-                  <Link
-                    href="/projects"
-                    activeClassName="is-active"
-                    className="nav-link"
-                    id="cardHover"
-                    onClick={closeShow}
-                    aria-label="Go To Projects Page"
-                    title="Projects"
-                  >
-                    Projects
-                  </Link>
-                </li>
-                <li className="nav-item hover__bottom">
-                  <Link
-                    href="/resume"
-                    activeClassName="is-active"
-                    className="nav-link"
-                    id="cardHover"
-                    onClick={closeShow}
-                    aria-label="Go To Resume Page"
-                    title="Resume"
-                  >
-                    Resume
-                  </Link>
-                </li>
-                <li className="nav-item hover__bottom">
-                  <Link
-                    href="/contact"
-                    activeClassName="is-active"
-                    className="nav-link"
-                    id="cardHover"
-                    onClick={closeShow}
-                    aria-label="Go To Contacts Page"
-                    title="Contact"
-                  >
-                    Contact
-                  </Link>
-                </li>
-                <li className="nav-item pl-md-3">
-                  <Link
-                    href="#!"
-                    className="nav-link nav-svg"
-                    aria-label={`Turn On ${theme === false ? 'Light' : 'Dark'
-                      } Mood`}
-                    onClick={setTheme}
-                  >
-                    <Moon />
-                  </Link>
-                </li>
-              </ul>
-            </div>
+              {/* Other Navigation Links */}
+              <li className="nav-item hover__bottom">
+                <Link href="/about" activeClassName="is-active" className="nav-link" id="cardHover" onClick={closeShow} aria-label="Go To About Page">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item hover__bottom">
+                <Link href="/projects" activeClassName="is-active" className="nav-link" id="cardHover" onClick={closeShow} aria-label="Go To Projects Page">
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item hover__bottom">
+                <Link href="/resume" activeClassName="is-active" className="nav-link" id="cardHover" onClick={closeShow} aria-label="Go To Resume Page">
+                  Resume
+                </Link>
+              </li>
+              <li className="nav-item hover__bottom">
+                <Link href="/contact" activeClassName="is-active" className="nav-link" id="cardHover" onClick={closeShow} aria-label="Go To Contact Page">
+                  Contact
+                </Link>
+              </li>
+
+              {/* Theme Toggle Button */}
+              <li className="nav-item pl-md-3">
+                <Link
+                  href="#!"
+                  className="nav-link nav-svg"
+                  aria-label={`Turn On ${theme === false ? "Light" : "Dark"} Mode`}
+                  onClick={setTheme}
+                >
+                  <Moon />
+                </Link>
+              </li>
+
+            </ul>
           </div>
-        </nav>
-      </Header>
-    </>
+        </div>
+      </nav>
+    </Header>
   );
 };
 
